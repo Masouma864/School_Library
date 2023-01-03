0162256cacf953df53b7e1ca4aa6890181c10e4d
masouma <masouma2020@gmail.com>
2023-01-03 20:36:36 +0430

    Fix linter error

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,5 @@
 class Nameable
-    def correct_name
-      raise NotImplementedError, 'Correctable'
-    end
+  def correct_name
+    raise NotImplementedError, 'Correctable'
   end
+end
--- person.rb
+++ person.rb
@@ -3,17 +3,17 @@ require_relative './capitalize'
 require_relative './trimmer'
 
 class Person < Nameable
-
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name='Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     super()
     @id = Random.rand(1..200)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
+
   def correct_name
     @correctable_name = name
   end
